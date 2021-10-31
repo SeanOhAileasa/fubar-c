43d6a8ed02d4dbeda4bbe5d89dfb895f7fb8c6bc
SeanOhAileasa <seanohaileasa@protonmail.com>
2021-10-31 10:48:51 +0000

    update repository ./fubar-c - Operators (relational/logical).

--- rc/tic/A02_Operators_Cast/t.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h> // call function rand
-#include <time.h> // call function time
-int main()
-{
-    srand((unsigned)time(NULL));
-    printf("%d\n",rand());
-    return 0;
-}
